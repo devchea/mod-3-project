c7904d366886a0a116a24f971abcfea52dc87732
devchea <rchea817@gmail.com>
2020-06-15 22:44:23 -0700

    first fighter dropdown added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,8 +5,9 @@ const urlMatches = "http://localhost:3000/matches";
 fetch(urlFighters)
  .then(res => res.json())
  .then(fighters => {
-     for(const fighter of fighters ){
+     for(const fighter of fighters){
          showFighter(fighter)
+         fighterDropdown(fighter)
      }
  })
 
@@ -17,5 +18,15 @@ function showFighter(fighter){
     const name = document.createElement('p')
     name.textContent = `${fighter.first_name} ${fighter.last_name}`;
 
-    
+
 }
+
+function fighterDropdown(fighter) {
+  const select = document.querySelector('#first-fighter')
+  const option = document.createElement('option')
+  option.value = `${fighter.first_name}-${fighter.last_name}`
+  option.innerHTML = `${fighter.first_name} ${fighter.last_name}`;
+  select.append(option)
+}
+
+
